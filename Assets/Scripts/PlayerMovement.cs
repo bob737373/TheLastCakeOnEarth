15cb94945083b739ae07898e213ccbbfe646a7d9
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2022-03-05 16:44:50 -0600

    Added player and basic player movement script. Attached camera to player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    [SerializeField]
+    float moveSpeed = 5f;
+    [SerializeField]
+    Rigidbody2D rb;
+
+    Vector2 movement;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() 
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
