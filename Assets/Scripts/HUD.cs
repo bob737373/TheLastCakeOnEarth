5e9ce4fc0e3b8a20e14653e850443e295badcbf5
bob737373 <rfahey2@lsu.edu>
2022-03-05 17:39:52 -0600

    Finished stock HUD elements, added HUD script

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HUDScript : MonoBehaviour
+public class HUD : MonoBehaviour
 {
-    
+    public Text coinsText;
+    public Text icingText;
+    public Image healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,18 @@ public class HUDScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void updateHealth(){
+
+    }
+
+    public void updateCoins(){
+
+    }
+
+    public void updateIcing(){
+
     }
 
 
