b762a8db81dd9bb07c0a3fe26f574772e9395174
Jonathan Dolbee <jonathandolbee@ymail.com>
2022-03-24 19:16:56 -0500

    - Enemy now chases the player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,12 +7,45 @@ public class Enemy : MonoBehaviour
 
     [SerializeField]
     int maxHealth;
+
+    [SerializeField]
+    float walkingSpeed = 1f;
+
     int health;
-    bool dead;
+    private Transform target = null;
+    private Vector2 movement;
+
+    [SerializeField]
+    float meleeAttackRange = 15.0f;
+
+    bool isDead;
+
+    private enum EnemyState
+    {
+        Idle,
+        moveToTarget,
+        moveToStartPosition
+    }
+
+    private EnemyState currentState;
+
+    private Vector3 startingPosition;
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    
+    private void walkTo(Vector3 destination)
+    {
+       transform.position = Vector2.Lerp(transform.position, destination, Time.deltaTime);
+    //    transform.position = Vector2.MoveTowards(this.transform.position, position, step);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        currentState = EnemyState.Idle;
+        startingPosition = this.transform.position;
         health = maxHealth;
     }
 
@@ -20,24 +53,88 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         
+        // TODO: Check if in attack range.
+        if (target)
+        {
+            currentState = EnemyState.moveToTarget;
+        }
+        else if (startingPosition != this.transform.position)
+        {
+            currentState = EnemyState.moveToStartPosition;
+        }
+        else
+        {
+            currentState = EnemyState.Idle;
+        }
+
+
+        handleState();
     }
 
-    protected virtual void attack() {
-        
+    void FixedUpdate()
+    {
+
     }
 
-    public void TakeDamage(int damage) {
+    private void handleState()
+    {
+        switch (currentState)
+        {
+            case EnemyState.Idle:
+                {
+                    print("idling");
+                    break;
+                }
+            case EnemyState.moveToStartPosition:
+                {
+                    print("Moving back to start position");
+                    walkTo(startingPosition);
+                    break;
+                }
+            case EnemyState.moveToTarget:
+                {
+                    walkTo(target.transform.position);
+                    break;
+                }
+            default: break;
+        }
+    }
+
+    protected virtual void attack()
+    {
+
+    }
+
+    public void TakeDamage(int damage)
+    {
         health -= damage;
-        if (health <= 0) {
+        if (health <= 0)
+        {
             Die();
         }
     }
 
-    void Die() {
+    void Die()
+    {
         print("died");
     }
 
-    bool canAttack() {
+    bool canAttack()
+    {   
+        // TODO: Check if in melee range.
         return true;
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player") target = other.transform;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player") target = null;
+    }
+
+
+
 }
--- Assets/Scripts/FriesEnemy.cs
+++ Assets/Scripts/FriesEnemy.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FriesEnemy : Enemy
-{
-    override protected void attack() {
-        
-    }
+{   
+    
+    
 }
