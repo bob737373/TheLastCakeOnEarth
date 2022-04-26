4216128ccba373444550bb30c79228d11cf2629e
Jonathan Dolbee <jonathandolbee@ymail.com>
2022-04-26 12:30:34 -0500

    Fixed main menu

--- Assets/MainMenuAssets/MainMenu.cs
+++ Assets/MainMenuAssets/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     
     public void StartGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("LoadScene");
     }
     
     public void ExitButton()
