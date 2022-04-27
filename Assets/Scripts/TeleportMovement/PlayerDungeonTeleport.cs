73f53cea3ad52a0a76e4ca76bf5f1cd9915f2bf5
Cultureddesert <lwfrick@gmail.com>
2022-04-26 19:33:00 -0500

    Adds teleporters for dungeon movement to emulate random generation without needing actual random generation

---
+++ Assets/Scripts/TeleportMovement/PlayerDungeonTeleport.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDungeonTeleport : MonoBehaviour
+{
+    private GameObject currentTeleporter;
+    private int floor;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (currentTeleporter != null)
+            {
+                //checks if the teleporter is set as an exit teleporter, which if it is, will go back to the previous room instead of the next room
+                if (currentTeleporter.GetComponent<DungeonTeleporter>().isExit() != true) {
+                    floor = currentTeleporter.GetComponent<DungeonTeleporter>().GetFloor();
+                    transform.position = currentTeleporter.GetComponent<DungeonTeleporter>().GetDestination(GetComponent<SimpleSeed>().getSeedFor(floor + 1)).position;
+                } else {
+                    floor = currentTeleporter.GetComponent<DungeonTeleporter>().GetFloor();
+                    transform.position = currentTeleporter.GetComponent<DungeonTeleporter>().GetDestination(GetComponent<SimpleSeed>().getSeedFor(floor - 1)).position;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            currentTeleporter = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            if (collision.gameObject == currentTeleporter)
+            {
+                currentTeleporter = null;
+            }
+        }
+    }
+}
